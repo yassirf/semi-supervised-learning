26ce439361515425e66330c40ad099221cbdfb11
yassirf <yf286@cam.ac.uk>
2022-04-18 21:15:19 +0100

    Update __init__.py

--- models/cifar/__init__.py
+++ models/cifar/__init__.py
@@ -2,3 +2,8 @@ from __future__ import absolute_import
 
 from .wideresnet import wideresnet282, wideresnet2810, wideresnet404
 from .wideresnet import linearwideresnet282, linearwideresnet2810, linearwideresnet404
+
+from .normalizingflow import InverseAutoregressiveFlow, RadialFlow
+
+from .pnwideresnet import iafwideresnet282, iafwideresnet2810, iafwideresnet404
+from .pnwideresnet import radialwideresnet282, radialwideresnet2810, radialwideresnet404
