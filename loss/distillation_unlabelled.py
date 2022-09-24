ca22efbf991a87c1fc1d17491585eb4d06c914f8
Adian Liusie <adianliusie98@gmail.com>
2022-09-24 20:11:30 +0100

    fixed namespace bug and unlabelled distillation now works

--- loss/distillation_unlabelled.py
+++ loss/distillation_unlabelled.py
@@ -23,32 +23,32 @@ class UnlabelledDistillation(Distillation):
         self.consistency_loss = kl_divergence_loss
 
         # whether to use training data or unseen unlabelled data for distillation
-        self.use_training_data = args.distil-train-data
+        self.use_training_data = args.distil_train_data
                                           
     def forward(self, info):
         # Get unlabelled batch
-        x_ul = info['x_ul'] if False else info['x']
-
+        x = info['x_ul'] if not self.use_training_data else info['x_l']
+        
         # Perform model forward pass
-        pred_ul, _ = self.model(x_ul)
+        pred, _ = self.model(x)
 
         with torch.no_grad():
             # The second input is for the teacher model
-            teacher_ul, _ = self.teacher(x_ul)
+            teacher_pred, _ = self.teacher(x)
 
         # Get the kl-loss averaged over batch
-        kd = self.consistency_loss(pred_ul, teacher_ul, self.distillation_t)
+        kd = self.consistency_loss(pred, teacher_pred, self.distillation_t)
 
         # Compute total loss
         loss = kd * self.distillation_t ** 2
 
-        # Compute accuracy
-        acc = accuracy(pred_ul.detach().clone(), y_l, top_k = (1, 5))
+        # Compute accuracy for logging
+        y = info['y_ul'] if not self.use_training_data else info['y_l']
+        acc  = accuracy(pred.detach().clone(), y, top_k = (1, 5))
 
         # Record metrics
         linfo = {'metrics': {
             'loss': loss.item(),
-            'ce': ce.item(),
             'kd': kd.item(),
             'acc1': acc[0].item(),
             'acc5': acc[1].item(),
